2020dd1dc3fc0b2809e14f09f3d6eb50b90360ca
cws <cwooseok60@gmail.com>
2020-06-14 17:01:06 +0900

    Initial commit

---
+++ src/main/java/com/springboot/calculationgame/CalculationGameApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.calculationgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculationGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/calculationgame/CalculationGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.calculationgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
