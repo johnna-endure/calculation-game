2d7dca1947c1524f08705c43f7fc20226b42d2eb
cws <cwooseok60@gmail.com>
2020-06-19 19:11:36 +0900

    GameController.loadGamePage 수정
    페이지 로드시 model에 username 속성 추가

--- src/main/java/com/springboot/calculationgame/controller/page/GameController.java
+++ src/main/java/com/springboot/calculationgame/controller/page/GameController.java
@@ -1,13 +1,23 @@
 package com.springboot.calculationgame.controller.page;
 
+import com.springboot.calculationgame.domain.user.User;
+import com.springboot.calculationgame.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class GameController {
 
-    @GetMapping("/game")
-    public String loadGamePage() {
+    private final UserService userService;
+
+    @GetMapping("/game/{id}")
+    public String loadGamePage(@PathVariable Long id, Model model) {
+        User user = userService.findUserById(id).get();
+        model.addAttribute("username", user.getUsername());
         return "game";
     }
 }
